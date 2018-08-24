20b371fc97312213b8601f448265b7bb29bcb491
Viktor Voronin <voroninman@gmail.com>
2018-08-24 09:24:53 +0200

    Add macOS.sh

---
+++ macOS.sh
@@ -0,0 +1,6 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+defaults write com.apple.dock persistent-apps '()'
+defaults write com.apple.dock persistent-others '()'
+defaults write com.apple.dock autohide -bool true
+killall Dock
+
