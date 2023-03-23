474fbc3aad8f8fd96f64b88a911b5737d9662f69
Viktor Voronin <voroninman@gmail.com>
2023-03-23 09:42:24 +0100

    Update macOS.sh

--- macOS.sh
+++ macOS.sh
@@ -8,6 +8,7 @@ defaults write com.apple.dock persistent-apps '()'
 defaults write com.apple.dock persistent-others '()'
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.Terminal AutoMarkPromptLines -int 0
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 killall Dock
 open /Applications/Utilities/Terminal.app/Contents/Resources/Fonts/
